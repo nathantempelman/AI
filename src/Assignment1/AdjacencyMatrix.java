de3d904c0517cdcf2808acb22446fb4d41a4dab8
Nathan Tempelman <nathantempelman@gmail.com>
2015-01-27 20:57:17 -0500

    Added naming for the nodes, and the ability to call searches by name. It's pretty hackey so far though.

--- src/Assignment1/AdjacencyMatrix.java
+++ src/Assignment1/AdjacencyMatrix.java
@@ -4,17 +4,16 @@ import java.util.LinkedList;
 
 public class AdjacencyMatrix {
 	int[][] mat;
+	String[] vNames;
 	int edges;
 	int vertices;
 	
+	// construct random AdjacencyMatrix
 	public AdjacencyMatrix(int vertices, int edges)
 	{
-		mat = new int[vertices][vertices];
-		this.vertices = vertices;
-		this.edges = 0;
-		
 		if(edges>vertices*vertices) throw new RuntimeException("Too many edges");
 		
+		initializeGraph(vertices);
 		while(this.edges<edges)
 		{
 			int a = (int)(vertices*Math.random());
@@ -26,6 +25,28 @@ public class AdjacencyMatrix {
 			}
 		}
 		
+	}
+	// takes an array of names for vertices - ["Vertex at Index 0's name", "Vertex at 1's name", ...]
+	// and a array of edges  				- [[Vertex A index, Vertex B index, edge weight],...] 
+	// it's kind of ugly honestly, but it'll work for now.
+	public AdjacencyMatrix(String[] vertices, int[][] edges)
+	{
+		initializeGraph(vertices.length);
+		vNames = vertices;
+
+		int i = 0;
+		while(this.edges<edges.length)
+		{
+			addUndirectedEdge(edges[i][0],edges[i][1],edges[i][2]);
+			i++;
+		}
+		
+	}
+	public void initializeGraph(int vertices)
+	{
+		mat = new int[vertices][vertices];
+		this.vertices = vertices;
+		this.edges = 0;
 	}
 	public void addUndirectedEdge(int a, int b, int weight)
 	{
@@ -46,10 +67,29 @@ public class AdjacencyMatrix {
 		}
 		return str.toString();
 	}
+	//this would be prettier with a hashmap
+	public int getIndexOf(String name)
+	{
+		for(int i = 0; i<vNames.length;i++)
+		{
+			if(vNames[i].equals(name))
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	public boolean breadthFirstSearch(String start, String end)
+	{
+		return breadthFirstSearch(getIndexOf(start),getIndexOf(end));
+	}
 	public boolean breadthFirstSearch(int start, int end)
 	{
 		LinkedList<Integer> q = new LinkedList<Integer>();
 		boolean[] checked = new boolean[vertices];
+		if(start == end)
+			return true;
+		checked[start]=true;
 		q.add(start);
 		
 		while(!q.isEmpty())
@@ -72,10 +112,17 @@ public class AdjacencyMatrix {
 		
 		return false;
 	}
+	public boolean depthFirstSearch(String start, String end)
+	{
+		return depthFirstSearch(getIndexOf(start),getIndexOf(end));
+	}
 	public boolean depthFirstSearch(int start, int end)
 	{
 		LinkedList<Integer> q = new LinkedList<Integer>();
 		boolean[] checked = new boolean[vertices];
+		if(start == end)
+			return true;
+		checked[start]=true;
 		q.add(start);
 		
 		while(!q.isEmpty())
