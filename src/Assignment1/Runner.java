2b124b4df872fe26049801fb332708ff569f66eb
Nathan Tempelman <nathantempelman@gmail.com>
2015-01-27 20:07:35 -0500

    'Second'

--- src/Assignment1/Runner.java
+++ src/Assignment1/Runner.java
@@ -3,10 +3,11 @@ package Assignment1;
 public class Runner {
 
 	public static void main(String[] args) {
-		AdjacencyMatrix m = new AdjacencyMatrix(5, 10);
+		AdjacencyMatrix m = new AdjacencyMatrix(5, 5);
 		System.out.print("matrix looks like\n");
 		System.out.print(m);
-		
+		System.out.println("1 - 4?"+m.breadthFirstSearch(1, 4));
+		System.out.println("Depth first agrees?"+m.depthFirstSearch(1, 4));
 	}
 
 }
