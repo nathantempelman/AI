89538b1efd20b2207f7da6408496d68fb2cf1fab
Nathan Tempelman <nathantempelman@gmail.com>
2015-01-30 15:28:34 -0500

    Added representation of Romanian cities and made the searches output paths

--- src/Assignment1/AdjacencyMatrix.java
+++ src/Assignment1/AdjacencyMatrix.java
@@ -1,5 +1,9 @@
 package Assignment1;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 
 public class AdjacencyMatrix {
@@ -112,6 +116,41 @@ public class AdjacencyMatrix {
 		
 		return false;
 	}
+	public String breadthFirstSearchWithPath(int start, int end)
+	{
+		LinkedList<Integer> q = new LinkedList<Integer>();
+		boolean[] checked = new boolean[vertices];
+		int[] visitedFrom = new int[vertices];
+		for(int i = 0;i<vertices;i++)
+		{
+			visitedFrom[i]=-1;
+		}
+		if(start == end)
+			return "Start and end are the same";
+		checked[start]=true;
+		q.add(start);
+		
+		while(!q.isEmpty())
+		{
+			int current = q.removeFirst();
+			if(current == end)
+			{
+				return turnVisitedArrayIntoString(start, end, visitedFrom);
+			}
+			for(int i = 0;i<vertices;i++)
+			{
+				
+				if(mat[current][i]!=0 && checked[i]==false )
+				{
+					checked[i]=true;
+					visitedFrom[i]=current;
+					q.add(i);
+				}
+			}
+		}
+		
+		return "No Path Possible";
+	}
 	public boolean depthFirstSearch(String start, String end)
 	{
 		return depthFirstSearch(getIndexOf(start),getIndexOf(end));
@@ -128,6 +167,103 @@ public class AdjacencyMatrix {
 		while(!q.isEmpty())
 		{
 			int current = q.removeLast();
+			if(current == end)
+			{
+				return true;
+			}
+			for(int i = 0;i<vertices;i++)
+			{
+				
+				if(mat[current][i]!=0 && checked[i]==false )
+				{
+					checked[i]=true;
+					q.add(i);
+				}
+			}
+		}
+		
+		return false;
+	}
+	public String depthFirstSearchWithPath(int start, int end)
+	{
+		LinkedList<Integer> q = new LinkedList<Integer>();
+		boolean[] checked = new boolean[vertices];
+		int[] visitedFrom = new int[vertices];
+		for(int i = 0;i<vertices;i++)
+		{
+			visitedFrom[i]=-1;
+		}
+		if(start == end)
+			return "Start and end are the same";
+		checked[start]=true;
+		q.add(start);
+		
+		while(!q.isEmpty())
+		{
+			int current = q.removeLast();
+			if(current == end)
+			{
+				return turnVisitedArrayIntoString(start, end, visitedFrom);
+			}
+			for(int i = 0;i<vertices;i++)
+			{
+				
+				if(mat[current][i]!=0 && checked[i]==false )
+				{
+					checked[i]=true;
+					visitedFrom[i]=current;
+					q.add(i);
+				}
+			}
+		}
+		
+		return "No Path Possible";
+	}
+	public String turnVisitedArrayIntoString(int start, int end, int[] visited)
+	{
+		List<Integer> path = new ArrayList<Integer>();
+		int current=end;
+		path.add(current);
+		while(true)
+		{
+			if(current!=start)
+			{
+				current = visited[current];
+				path.add(current);
+			}
+			else
+			{
+				break;
+			}
+		}
+		Collections.reverse(path);
+		StringBuilder b = new StringBuilder();
+		b.append("Path is ");
+		Iterator<Integer> i = path.iterator();
+		while(true)
+		{
+			b.append(i.next());
+			if(i.hasNext())
+				b.append(" -> ");
+			else
+				break;
+		}
+		return b.toString();
+	}
+	// cost     [ , , ]
+	// frontier [ , , ]
+	public boolean uniformCostSearch(int start, int end)
+	{
+		LinkedList<Integer> q = new LinkedList<Integer>();
+		boolean[] checked = new boolean[vertices];
+		if(start == end)
+			return true;
+		checked[start]=true;
+		q.add(start);
+		
+		while(!q.isEmpty())
+		{
+			int current = q.removeFirst();
 			if(current == end)
 			{
 				return true;
--- src/Assignment1/Runner.java
+++ src/Assignment1/Runner.java
@@ -3,11 +3,32 @@ package Assignment1;
 public class Runner {
 
 	public static void main(String[] args) {
-		AdjacencyMatrix m = new AdjacencyMatrix(5, 5);
+		AdjacencyMatrix m = new AdjacencyMatrix(400, 30000);
 		System.out.print("matrix looks like\n");
 		System.out.print(m);
 		System.out.println("1 - 4?"+m.breadthFirstSearch(1, 4));
 		System.out.println("Depth first agrees?"+m.depthFirstSearch(1, 4));
+		System.out.println("And now with the dfs path: "+m.depthFirstSearchWithPath(1, 4));
+		System.out.println("And now with the bfs path: "+m.breadthFirstSearchWithPath(1, 4));
+		
+		AdjacencyMatrix m2 = createRomania();
+		System.out.println(m2);
+		System.out.println("bfs path from Oradea to Urziceni"+m2.breadthFirstSearchWithPath(0, 14));
+		System.out.println("dfs path from Oradea to Urziceni"+m2.depthFirstSearchWithPath(0, 14));
+	}
+
+	private static AdjacencyMatrix createRomania() {
+		String[] cityNames = {"Oradea","Zerind","Arad","Sibiu","Timisoara",
+							"Lugoj","Mehadia","Drobeta","Craiova","Rimnieu Vilcea",
+							"Fagaras","Pitesti","Bucharest","Giurgiu","Urziceni",
+							"Hirsova","Eforie","Vaslui","Iasi","Neamt"};
+		int[][] edges = {{0,1,71},{0,3,151},{1,2,75},{2,3,140},{2,4,118},{4,5,111},
+						{5,6,70},{6,7,75},{7,8,120},{8,9,146},{8,11,138},{3,10,99},
+						{9,3,80},{9,11,97},{11,12,101},{12,13,90},{12,14,85},{10,12,211},
+						{14,15,98},{15,16,86},{14,17,142},{17,18,92},{18,19,87}};
+		
+		
+		return new AdjacencyMatrix(cityNames,edges);
 	}
 
 }
